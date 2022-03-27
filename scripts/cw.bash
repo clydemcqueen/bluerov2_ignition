6022e64eb2b3ccd47706547a5603cc898c7a20c4
Clyde McQueen <clyde@mcqueen.net>
2022-03-26 18:26:07 -0700

    Tweaks from testing w/ SIM_Submarine and SIM_JSON

--- scripts/cw.bash
+++ scripts/cw.bash
@@ -1,5 +1,5 @@
 # Sends to cmd_thrust
-ign topic -t /model/bluerov2/joint/thruster1_joint/cmd_thrust -m ignition.msgs.Double -p 'data: -5'
-ign topic -t /model/bluerov2/joint/thruster2_joint/cmd_thrust -m ignition.msgs.Double -p 'data: 5'
-ign topic -t /model/bluerov2/joint/thruster3_joint/cmd_thrust -m ignition.msgs.Double -p 'data: 5'
-ign topic -t /model/bluerov2/joint/thruster4_joint/cmd_thrust -m ignition.msgs.Double -p 'data: -5'
+ign topic -t /model/bluerov2/joint/thruster1_joint/cmd_thrust -m ignition.msgs.Double -p 'data: 5'
+ign topic -t /model/bluerov2/joint/thruster2_joint/cmd_thrust -m ignition.msgs.Double -p 'data: -5'
+ign topic -t /model/bluerov2/joint/thruster3_joint/cmd_thrust -m ignition.msgs.Double -p 'data: -5'
+ign topic -t /model/bluerov2/joint/thruster4_joint/cmd_thrust -m ignition.msgs.Double -p 'data: 5'
---
+++ scripts/up.bash
@@ -0,0 +1,3 @@
+# Sends to cmd_thrust
+ign topic -t /model/bluerov2/joint/thruster5_joint/cmd_thrust -m ignition.msgs.Double -p 'data: -5'
+ign topic -t /model/bluerov2/joint/thruster6_joint/cmd_thrust -m ignition.msgs.Double -p 'data: -5'
