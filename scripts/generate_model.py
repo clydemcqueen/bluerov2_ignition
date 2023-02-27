a58ad87d5730f6ff2e9480ac8c0c9bd1bca3d6d2
Clyde McQueen <clyde@mcqueen.net>
2023-02-27 15:33:11 -0800

    Add a Ping sonar sensor

--- scripts/generate_model.py
+++ scripts/generate_model.py
@@ -14,6 +14,10 @@ supports 2 control methods:
 The ThrusterPlugin uses the Fossen equation to relate thrust to angular velocity, and will apply
 thrust force to the joint and spin the propeller. Propellers have bounding boxes and inertia, so
 spinning the propeller does affect the simulation.
+
+Typical usage:
+scripts/generate_model.py models/bluerov2/model.sdf.in models/bluerov2/model.sdf
+scripts/generate_model.py models/br2_ping/model.sdf.in models/br2_ping/model.sdf --ping
 """
 
 import math
@@ -26,7 +30,7 @@ d90 = d180 / 2
 d45 = d90 / 2
 d135 = d90 + d45
 
-mass = 10
+base_mass = 10
 visual_x = 0.457
 visual_y = 0.338
 visual_z = 0.25
@@ -34,24 +38,18 @@ fluid_density = 1000
 dome_x = visual_x / 2
 
 # The ROV should be positively buoyant
-buoyancy_adjustment = 0.05
-displaced_mass = mass + buoyancy_adjustment
+buoyancy_adjustment = 0.038
 
 # The collision box is used by the BuoyancyPlugin
 # collision_x * collision_y * collision_z * density == displaced_mass
 collision_x = visual_x
 collision_y = visual_y
-collision_z = displaced_mass / (visual_x * visual_y * fluid_density)
 
 # The center of mass is just above the origin
 mass_z = 0.011
 
 # The center of volume is directly above the center of mass, resulting in a restoring force
 volume_z = 0.06
-
-ixx = mass / 12 * (collision_y * collision_y + collision_z * collision_z)
-iyy = mass / 12 * (collision_x * collision_x + collision_z * collision_z)
-izz = mass / 12 * (collision_x * collision_x + collision_y * collision_y)
 
 # 2nd order stability for the HydrodynamicsPlugin
 xUabsU = -0.5 * visual_y * visual_z * 0.8 * fluid_density
@@ -67,6 +65,13 @@ thruster_y = 0.09
 thruster_z = -0.009
 vert_thruster_y = 0.105
 vert_thruster_z = 0.09
+
+# Ping sonar
+ping_mass = 0.133
+ping_radius = 0.024
+ping_length = 0.035
+ping_x = -0.18
+ping_z = -0.095
 
 # Propeller link parameters
 propeller_size = "0.1 0.02 0.01"
@@ -90,6 +95,15 @@ control_offset = -0.5
 
 # From the command line
 use_angvel_cmd = False
+has_ping_sonar = False
+
+# Mass changes if we add a Ping sonar, so these vars are set by update_globals()
+total_mass = 0
+displaced_mass = 0
+collision_z = 0
+ixx = 0
+iyy = 0
+izz = 0
 
 # Set by update_globals()
 cw_control_multiplier = 0   # Thrusters 3, 4 and 6
@@ -119,6 +133,13 @@ def update_globals():
     global thruster5_topic
     global thruster6_topic
 
+    global total_mass
+    global displaced_mass
+    global collision_z
+    global ixx
+    global iyy
+    global izz
+
     if use_angvel_cmd:
         print("control method: angular velocity")
         thruster1_topic += "vel"
@@ -146,6 +167,19 @@ def update_globals():
         cw_control_multiplier = max_thrust * 2
         ccw_control_multiplier = max_thrust * 2
 
+    if has_ping_sonar:
+        print("ping sonar: True")
+        total_mass = base_mass + 6 * propeller_mass + ping_mass
+    else:
+        print("ping sonar: False")
+        total_mass = base_mass + 6 * propeller_mass
+
+    displaced_mass = total_mass + buoyancy_adjustment
+    collision_z = displaced_mass / (visual_x * visual_y * fluid_density)
+    ixx = base_mass / 12 * (collision_y * collision_y + collision_z * collision_z)
+    iyy = base_mass / 12 * (collision_x * collision_x + collision_z * collision_z)
+    izz = base_mass / 12 * (collision_x * collision_x + collision_y * collision_y)
+
 
 def generate_model(input_path, output_path):
     s = open(input_path, "r").read()
@@ -157,14 +191,20 @@ def generate_model(input_path, output_path):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    if len(sys.argv) < 3:
         print("Usage:")
-        print("generate_model.py infile outfile 0|1")
-        print("0: control thrust force")
-        print("1: control angular velocity")
+        print("generate_model.py infile outfile [--angular] [--ping]")
         exit(-100)
 
-    use_angvel_cmd = bool(int(sys.argv[3]))
+    if len(sys.argv) > 3:
+        for arg in sys.argv[3:]:
+            if arg == '--angular':
+                use_angvel_cmd = True
+            elif arg == '--ping':
+                has_ping_sonar = True
+            else:
+                print(f'invalid argument "{arg}"')
+                exit(-101)
 
     update_globals()
 
